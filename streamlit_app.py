a380b72f80c45611b5d7ea8d674a1555c3040a42
kamudinho <kasper@kaspermundt.dk>
2024-07-03 15:29:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Kasper er for god")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Jeg laver lige en test/)."
 )
